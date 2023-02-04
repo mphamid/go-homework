991d7e5f5c770157695c443bddff0388f12b8ea5
Hamid Mahmoudpour <mp.hamid@yahoo.com>
2023-02-04 04:05:15 +0330

    yaml reader

---
+++ main.go
@@ -0,0 +1,113 @@
+package main
+
+import (
+	"log"
+	"os"
+	"regexp"
+	"strconv"
+	"strings"
+)
+
+type parameter struct {
+	name  string
+	value string
+}
+
+func main() {
+	file, err := os.ReadFile("conf.yaml")
+	if err != nil {
+		stopWithError("yaml file not found")
+	}
+	yamlContent := removeComment(string(file))
+	yamlMap := findSections(yamlContent)
+	server, serverFound := yamlMap["server"]
+	if serverFound == false {
+		stopWithError("server section must be exist")
+	}
+	validateServer(server)
+	hostParameter := findParameter(server, "host")
+	if hostParameter.name == "" {
+		yamlMap["server"] = append(yamlMap["server"], parameter{"host", "localhost"})
+	}
+	rateLimits, rateLimitsFound := yamlMap["rate_limits"]
+	if rateLimitsFound == true {
+		validateRateLimit(rateLimits)
+	}
+
+	log.Println("Configuration file is OK")
+	log.Println(yamlMap)
+}
+func findParameter(limits []parameter, pname string) parameter {
+	for _, limit := range limits {
+		if limit.name == pname {
+			return limit
+		}
+	}
+	return parameter{}
+}
+
+func validateRateLimit(limits []parameter) {
+	ipParameter := findParameter(limits, "ip_requests_per_sec")
+	if ipParameter.name == "" {
+		stopWithError("ip_requests_per_sec parameter is required in rate_limits section")
+	} else {
+		ipValue, err := strconv.Atoi(ipParameter.value)
+		if err != nil {
+			stopWithError("ip_requests_per_sec parameter is not a number")
+		}
+		if ipValue > 1000 || ipValue < 60 {
+			stopWithError("ip_requests_per_sec parameter must be <1000 and >60")
+		}
+	}
+	otpParameter := findParameter(limits, "otp_sms_interval_sec")
+	if otpParameter.name != "" {
+		otpValue, err := strconv.Atoi(otpParameter.value)
+		if err != nil {
+			stopWithError("otp_sms_interval_sec parameter is not a number")
+		}
+		if otpValue > 300 || otpValue <= 60 {
+			stopWithError("otp_sms_interval_sec parameter must be <300 and >=60")
+		}
+	}
+}
+
+func validateServer(server []parameter) {
+	portParameter := findParameter(server, "port")
+	if portParameter.name == "" {
+		stopWithError("port parameter is required in server section")
+	} else {
+		portRegex := regexp.MustCompile(`([1-9][0-9]{0,3}|[1-5][0-9]{4}|6[0-4][0-9]{3}|65[0-4][0-9]{2}|655[0-2][0-9]|6553[0-5])`)
+		if portRegex.ReplaceAllString(portParameter.value, "") != "" {
+			stopWithError("port parameter is not valid number")
+		}
+	}
+}
+func removeComment(inputString string) string {
+	return regexp.MustCompile(`(?m)#(.*)`).ReplaceAllString(inputString, "")
+}
+
+func findSections(inputString string) map[string][]parameter {
+	var sectionRegex = regexp.MustCompile(`([a-z_\d]+ *: *\n)( {2,}[a-z_\d]+ *: *(.*)\n*)*`)
+	var titleRegex = regexp.MustCompile(`([a-z_0-9]+) *: *\n`)
+	var subSectionRegex = regexp.MustCompile(` {2,}([a-z_0-9]+ *: *(.*)\n{0,})*`)
+	var title string
+	var splitParameter []string
+	all := make(map[string][]parameter)
+	sections := sectionRegex.FindAllString(inputString, -1)
+	for _, section := range sections {
+		title = trimSection(titleRegex.FindString(section))
+		for _, subSection := range subSectionRegex.FindAllString(section, -1) {
+			splitParameter = strings.Split(subSection, ":")
+			all[title] = append(all[title], parameter{trimSection(splitParameter[0]), trimSection(splitParameter[1])})
+		}
+	}
+	return all
+}
+func trimSection(str string) string {
+	return strings.Trim(str, ":\n ")
+}
+
+func stopWithError(message string) {
+	log.Fatal(message)
+	panic(message)
+}
